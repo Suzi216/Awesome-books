70aeaefb3ae65d6066c019b23a285c85141c4566
Suzi216 <66145033+Suzi216@users.noreply.github.com>
2022-08-08 12:25:48 +0300

    Factoring to use classes

--- index.js
+++ index.js
@@ -1,50 +1,54 @@
-let books=[];
-const bookdiv=document.querySelector(".books-list");
-const newtitle=document.querySelector('.book-name');
-const newauthor=document.querySelector(".book-author");
-const addbutton=document.querySelector(".add-book");
+// var books=[];
+const bookdiv = document.querySelector('.books-list');
+const newtitle = document.querySelector('.book-name');
+const newauthor = document.querySelector('.book-author');
+const addbutton = document.querySelector('.add-book');
 
-class Books {
-  constructer(){
-    // books = [];
-   var  books= new Array();
+class Book {
+  constructor() {
+    this.Books = [];
   }
-   //Add a book
-   addup(obj) {
-    this.books.push(obj);
-}
-    ///display the books in html page
-    display(){
-      bookdiv.innerHTML = '';
-      for(let i=0;i<this.books.length;i+=1){
-        const listofbook=document.createElement('div');
-        listofbook.classList.add('book_div');
-        listofbook.innerHTML = `<h4>${this.books[i].title}</h4>
-            <h4>${this.books[i].author}</h4>
+
+  // Add a book
+  addup(obj) {
+    this.Books.push(obj);
+  }
+
+  /// display the books in html page
+  display() {
+    bookdiv.innerHTML = '';
+    for (let i = 0; i < this.Books.length; i += 1) {
+      const listofbook = document.createElement('div');
+      listofbook.classList.add('book_div');
+      listofbook.innerHTML = `<h4>${this.Books[i].title}</h4>
+            <h4>${this.Books[i].author}</h4>
             <button id=${i} class="rem-btn">Remove</button><br> <br>`;
-        bookdiv.appendChild(listofbook);
-      }
+      bookdiv.appendChild(listofbook);
     }
-    // remove the books
-    removebook(id){
-      this.books.splice(id, 1);
-      display();
-    }
+  }
+
+  // remove the books
+  removebook(id) {
+    this.Books.splice(id, 1);
+    window.localStorage.setItem('books', JSON.stringify(this.Books));
+    this.display();
+  }
 }
 
-const bookuse= new Books();
-//adding books to the object book.
-addbutton.addEventListener("click", ()=> {
+const bookuse = new Book();
+// adding books to the object book.
+addbutton.addEventListener('click', () => {
   const book = { title: '', author: '' };
-  book.title=newtitle.value;
-  book.author=newauthor.value;
+  book.title = newtitle.value;
+  book.author = newauthor.value;
   bookuse.addup(book);
+  window.localStorage.setItem('books', JSON.stringify(this.Books));
   bookuse.display();
-})
+});
 
 // remove the books from list
 bookdiv.addEventListener('click', (e) => {
-  if (event.target.classList.contains('rem-btn')){
+  if (e.target.classList.contains('rem-btn')) {
     bookuse.removebook(e.target.id);
   }
 });
